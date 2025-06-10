81c1ed1708e6d08c5f4c0640cc9dc3feee7c2740
korimuflower <33154027+korimuflower@users.noreply.github.com>
2025-06-10 18:08:04 +0900

    feature - ドロップダウンリストのテストを追加、不要なディレクトリが作成されていたので削除

--- outputs/playwright/theInternet/addRemoveElements.test.ts
+++ outputs/playwright/theInternet/addRemoveElements.test.ts
@@ -6,7 +6,11 @@ test('要素の追加・削除ができること。', async ({ page }) => {
   await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
 
   await page.getByRole('button', { name: 'Add Element' }).click();
+
+  // 削除ボタンを押せる＝追加に成功したとみなす
   await page.getByRole('button', { name: 'Delete' }).click();
+
+  // 削除ボタンが非表示＝削除に成功したことを確認
   await expect(page.getByRole('button', { name: 'Delete' })).toBeHidden();
 
 });
