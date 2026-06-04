83fb0d93c4df015a5cb43dafd44182ef99385d6e
korimuflower <33154027+korimuflower@users.noreply.github.com>
2026-06-04 22:00:48 +0900

    メールアドレスの生成部分を追加

--- outputs/playwright/hotel-planisphere/tests/auth/registration_premium.spec.ts
+++ outputs/playwright/hotel-planisphere/tests/auth/registration_premium.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-    // ランダムな文字列を生成する関数
+    // 指定した文字数でランダムな文字列を生成する関数
     // 今後共通関数として抽出予定
 
     function randomAlphaNumeric(length: number): string {
@@ -10,9 +10,15 @@ import { test, expect } from '@playwright/test';
 
 test('プレミアムアカウントを新規作成できること。',async({page}) => {
 
+    // ダミーのメールアドレス・パスワード・氏名を生成
+    // 任意項目である住所・電話番号・性別・生年月日は入力しない
+
+    const registrationMailaddress = `${randomAlphaNumeric(10)}@example.com`;
+
+    // 改修により『会員登録』の文言が複数表示される可能性もあるので、ナビゲーションバーをロケーターとして指定
     await page.goto('/ja/');
+    await page.locator('nav').getByRole('button', { name: '会員登録'}).click();
 
-
+    await page.getByLabel('メールアドレス').fill(registrationMailaddress);
 
 });
-
