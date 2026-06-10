e7ce7e26a80da75d97a085c3ecee0b49e21b14d7
korimuflower <33154027+korimuflower@users.noreply.github.com>
2026-06-10 21:48:53 +0900

    登録ボタンを押すところまで追加。確認ステップは次コミットで対応

--- outputs/playwright/hotel-planisphere/tests/auth/registration_premium.spec.ts
+++ outputs/playwright/hotel-planisphere/tests/auth/registration_premium.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
     // 指定した文字数でランダムな文字列を生成する関数
-    // 今後共通関数として抽出予定
+    // 共通関数として抽出予定
 
     function randomAlphaNumeric(length: number): string {
         const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -14,11 +14,18 @@ test('プレミアムアカウントを新規作成できること。',async({pa
     // 任意項目である住所・電話番号・性別・生年月日は入力しない
 
     const registrationMailaddress = `${randomAlphaNumeric(10)}@example.com`;
+    const registrationPassword = `${randomAlphaNumeric(12)}`;
+    const registrationName = `${randomAlphaNumeric(10)}dummyname`;
 
     // 改修により『会員登録』の文言が複数表示される可能性もあるので、ナビゲーションバーをロケーターとして指定
     await page.goto('/ja/');
     await page.locator('nav').getByRole('link', { name: '会員登録'}).click();
 
     await page.getByLabel('メールアドレス').fill(registrationMailaddress);
+    await page.getByLabel('パスワード 必須').fill(registrationPassword);
+    await page.getByLabel('パスワード（確認） 必須').fill(registrationPassword);
+    await page.getByRole('button',{ name: '登録' }).click();
+
+    // ToDo:登録後の画面表示を確認し、作成が成功したことを示す
 
 });
